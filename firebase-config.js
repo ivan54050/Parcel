996f2b54fa85c9cba3b40bc4f6400e0813fd8c30
ivan54050 <vmanilik@gmail.com>
2025-07-21 15:19:49 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjLuotxHzCIkSLduDlLb3AYTc6A6A8NYc",
+  authDomain: "parcelapp-900b9.firebaseapp.com",
+  projectId: "parcelapp-900b9",
+  storageBucket: "parcelapp-900b9.appspot.com",
+  messagingSenderId: "310089427450",
+  appId: "1:310089427450:web:7cf3dc742c313d0f28dc43",
+  measurementId: "G-X33ZE00CYT"
+};
