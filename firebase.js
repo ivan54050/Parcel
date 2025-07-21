48b2a6cf8eb9427fdfa40dacca22001cc8f5fec6
ivan54050 <vmanilik@gmail.com>
2025-07-21 13:10:24 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
@@ -8,8 +9,8 @@ const firebaseConfig = {
   storageBucket: "parcelapp-900b9.appspot.com",
   messagingSenderId: "310089427450",
   appId: "1:310089427450:web:a3b6dcd22aee438028dc43",
-  measurementId: "G-LD8DF4V543"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+export { db };
